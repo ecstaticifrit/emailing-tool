7ead6eb18fff1ea9205d163cf366983286b4f021
ecstaticifrit <souptikghosh53@gmail.com>
2026-03-26 19:26:54 +0530

    changed mail client

--- app/email_sender.py
+++ app/email_sender.py
@@ -1,14 +1,20 @@
 import smtplib
 from email.mime.text import MIMEText
 from app.config import SMTP_HOST, SMTP_PORT, SMTP_EMAIL, SMTP_PASSWORD
+import mailtrap as mt
+
+client = mt.MailtrapClient(token="3d0f8962ea9141371c1275a48a7633b8")
+
 
 def send_email(to_email, subject, html_body):
-    msg = MIMEText(html_body, "html")
-    msg["Subject"] = subject
-    msg["From"] = SMTP_EMAIL
-    msg["To"] = to_email
 
-    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
-        server.starttls()
-        server.login(SMTP_EMAIL, SMTP_PASSWORD)
-        server.send_message(msg)
+    mail = mt.Mail(
+        sender=mt.Address(email="hello@demomailtrap.co", name="Mailtrap Test"),
+        to=[mt.Address(email=to_email)],
+        subject=subject,
+        text=html_body,
+        category="Integration Test",
+    )
+
+    response = client.send(mail)
+    print(response)
